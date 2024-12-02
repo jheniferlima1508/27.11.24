152d2f517b929fda9ad12ca0272578bdd1850f8d
jhenifer lima <jhenifer.g.lima@aluno.senai.br>
2024-12-02 11:05:20 -0500

    logica da situação

--- media.py
+++ media.py
@@ -7,3 +7,23 @@ n4 = float(input())
 faltas = int(input())
 #calculo da média
 media = (n1+n2+n3+n4)/4
+# logica da situação
+if faltas > 31:
+    situacao = "reprovado por falta"
+elif media >= 8:
+    situacao = "aprovado"
+elif media >= 5: #recuperação
+    recuperacao = float(input())#ler a nota da prova de recuperação
+    if recuperacao >= (10-media):
+     situacao = "aprovado na recuperação"
+    else:
+     situacao = "reprovado na recuperação"
+else:
+   situacao = "reprovado por média"
+#relatorio
+print("nome:",nome)
+print("notas:",n1,n2,n3,n4)
+print("faltas:",faltas)
+print("media:",media)
+print("situacao:",situacao)
+
