4fceed5ff3083a4e62b46e8abc44eac65a1a2f95
jhenifer lima <jhenifer.g.lima@aluno.senai.br>
2024-12-02 09:46:49 -0500

    definir critérios

---
+++ media.py
@@ -0,0 +1,9 @@
+#ler entrada do usuario
+nome = input() #armazenar o nome do aluno
+n1 = float(input()) # 4 notas do aluno
+n2 = float(input())
+n3 = float(input())
+n4 = float(input())
+faltas = int(input())
+#calculo da média
+media = (n1+n2+n3+n4)/4
