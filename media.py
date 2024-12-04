0017739d6ecfac50e2cf72d194737b9f3ba2fb1a
jhenifer lima <jhenifer.g.lima@aluno.senai.br>
2024-12-04 09:49:16 -0500

    concluido

--- media.py
+++ media.py
@@ -1,36 +1,39 @@
 #o codigo lendo varios alunos e a mesma coisa
-#a unica diferença e que ele esta de uma repetição de um enquanto
+#a unica diferença e que ele esta de uma repetição
+# de um enquanto
 #ler entrada do usuario
-cont = 0 # variavel que controla a repetição
-escolha_usuario = int(input()) #variavel que guarda quantas vezes o codigo vai rodar
+situacao = ""
+alunos = [] #lista que guardara todos os alunos cadastrado
+cont = 0 #variavel que controla a repetição
+escolha_usuario = int(input("quantos usuarios você quer cadastrar: ")) #variavel que guarda quantas vezes o codigo vai rodar
 while cont < escolha_usuario:
-    nome = input() #armazenar o nome do aluno
-    n1 = float(input()) # 4 notas do aluno
-    n2 = float(input())
-    n3 = float(input())
-    n4 = float(input())
+    nome = input("digite o nome do aluno")
+    faltas = float(input("digite as faltas do aluno"))
+    #4 notas 
+    n1 = float(input("digite a primeira nota")) 
+    n2 = float(input("digite a segunda nota"))
+    n3 = float(input("digite a terceira nota"))
+    n4 = float(input("digite a quarta nota"))
 
-    faltas = int(input())
-    #calculo da média
+  #calculo da média
     media = (n1+n2+n3+n4)/4
+
     # logica da situação
     if faltas > 31:
         situacao = "reprovado por falta"
     elif media >= 8:
         situacao = "aprovado"
     elif media >= 5: #recuperação
-        recuperacao = float(input())#ler a nota da prova de recuperação
+        recuperacao = float(input("quanto o aluno tirou na recuperação: "))#ler a nota da prova de recuperação
         if recuperacao >= (10-media):
          situacao = "aprovado na recuperação"
         else:
          situacao = "reprovado na recuperação"
     else:
-     situacao = "reprovado por média"
-    #relatorio
-    print("nome:",nome)
-    print("notas:",n1,n2,n3,n4)
-    print("faltas:",faltas)
-    print("media:",media)
-    print("situacao:",situacao)
-    cont = cont + 1 #
+        situacao = "reprovado por média"
 
+    #enviar os dados do aluno para a lista alunos
+    alunos.append([nome,faltas,media,situacao])
+    #relatorio
+    cont += 1
+print(alunos)
